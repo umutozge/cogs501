1950f5f683d90c703b415bebe8ccfecdc455a0b4
Umut Özge <umut.oezge@gmail.com>
2025-11-16 10:18:52 +0300

    docs first

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,34 @@
+var store = [{
+        "title": "Iteration by `while`",
+        "excerpt":"Let’s start with a simple function defined over integers called the Collatz function \\(C\\). \\[C(n) = \\begin{cases} \\frac{n}{2} &amp; \\text{if } n \\text{ is even}\\\\ 3n + 1 &amp; \\text{if } n \\text{ is odd} \\end{cases}\\] Collatz’ conjecture states that for any positive integer \\(n\\), repeated application of \\(C\\) will...","categories": ["Blog"],
+        "tags": ["programming","iteration","while loop"],
+        "url": "/blog/post-iteration-by-while/",
+        "teaser": null
+      },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "/404.html"
+  },{
+    "title": "About",
+    "excerpt":"Tempor velit sint sunt ipsum tempor enim ad qui ullamco. Est dolore anim ad velit duis dolore minim sunt aliquip amet commodo labore. Ut eu pariatur aute ea aute excepteur laborum. Esse ea esse excepteur minim mollit qui cillum excepteur ex dolore magna. Labore deserunt fugiat incididunt incididunt sint ea....","url": "/sbout/"
+  },{
+    "title": "Posts by Category",
+    "excerpt":" ","url": "/categories/"
+  },{
+    "title": "Programming exercises on iteration",
+    "excerpt":"The factorial function is defined over the non-negative integers as:1 \\[n! = \\begin{cases} 1, &amp; \\text{if } n = 0;\\\\ n \\times (n - 1)!, &amp; \\text{if } n &gt; 0. \\end{cases}\\] Define a function that computes the factorial of a given non-negative integer \\(n\\) using iteration by while. def...","url": "/ex_iteration/"
+  },{
+    "title": "COGS 501 Quiz",
+    "excerpt":"[2pts*] Translate the following into first order logic: i. Every student in this class has submitted at least one assignment. - S(x): “x is a student in this class” - A(x, y): “x has submitted assignment y” \\[\\forall x (S(x) \\to \\exists y A(x,y))\\] ii. There is a professor who...","url": "/q06s/"
+  },{
+    "title": "Schedule",
+    "excerpt":"Week Reading Quiz :calendar: 2/10     :calendar: 9/10     :calendar: 23/10     :calendar: 30/10     :calendar: 6/11   quiz :calendar: 13/11 CP 1.5, Epp 3, sections on Prolog can be omitted. No need for solutions. :calendar: 20/11     :calendar: 27/11     :calendar: 4/12  ...","url": "/schedule/"
+  },{
+    "title": "Syllabus",
+    "excerpt":"Prerequisites The course is open, without any prerequisites, to any graduate student, with Cogsci first, Informatics second, and the rest third priority. Material Composing Programs Epp, Susanna S. (2018). Discrete Mathematics with Applications. 5th ed., Cengage Learning. Everything that is discussed in the class. Requirements Weekly quizzes Midterm exam Final...","url": "/syllabus/"
+  },{
+    "title": "Posts by Tag",
+    "excerpt":" ","url": "/tags/"
+  },{
+    "title": "Posts by Year",
+    "excerpt":" ","url": "/posts/"
+  }]

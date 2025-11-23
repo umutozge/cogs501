da8254c145435fa39dd3184e83a1e13b4686c81f
Umut Özge <umut.oezge@gmail.com>
2025-11-23 11:27:02 +0300

    bugra 3

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,46 @@
+var store = [{
+        "title": "Iteration by `while`",
+        "excerpt":"Let’s start with a simple function defined over integers called the Collatz function \\(C\\). \\[C(n) = \\begin{cases} \\frac{n}{2} &amp; \\text{if } n \\text{ is even}\\\\ 3n + 1 &amp; \\text{if } n \\text{ is odd} \\end{cases}\\] Collatz’ conjecture states that for any positive integer \\(n\\), repeated application of \\(C\\) will...","categories": ["Blog"],
+        "tags": ["programming","iteration","while loop","cogs501"],
+        "url": "/docs/blog/2025/11/13/iteration-by-while.html",
+        "teaser": null
+      },{
+        "title": "Higher order functions",
+        "excerpt":"Remember our earlier discussion of Collatz function and sequences. Here is our task: Task: Given a positive integer \\(n\\), find the largest integer in the Collatz sequence seeded by \\(n\\).1 By now, the task should be a piece of cake for you. Is it? Try and see before proceeding. Here...","categories": ["Blog"],
+        "tags": ["programming","functional programming","cogs501"],
+        "url": "/docs/blog/2025/11/21/higher-order-functions.html",
+        "teaser": null
+      },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "https://umutozge.github.io/docs/404.html"
+  },{
+    "title": "About",
+    "excerpt":"Tempor velit sint sunt ipsum tempor enim ad qui ullamco. Est dolore anim ad velit duis dolore minim sunt aliquip amet commodo labore. Ut eu pariatur aute ea aute excepteur laborum. Esse ea esse excepteur minim mollit qui cillum excepteur ex dolore magna. Labore deserunt fugiat incididunt incididunt sint ea....","url": "https://umutozge.github.io/docs/sbout/"
+  },{
+    "title": "Posts by Category",
+    "excerpt":" ","url": "https://umutozge.github.io/docs/categories/"
+  },{
+    "title": "Exercises on higher-order functions",
+    "excerpt":"Define a function apply_twice that takes a function f and a value x, and returns the result of applying f to x two times. def sqr(x): return x * x def apply_twice(f, x): \"\"\"apply f to x twice. &gt;&gt;&gt; apply_twice(sqr, 3) 81 \"\"\" Define a function applier that applies a...","url": "https://umutozge.github.io/docs/_pages/exercises/higher-order-functions.html"
+  },{
+    "title": "Programming exercises on iteration",
+    "excerpt":"The factorial function is defined over the non-negative integers as:1 \\[n! = \\begin{cases} 1, &amp; \\text{if } n = 0;\\\\ n \\times (n - 1)!, &amp; \\text{if } n &gt; 0. \\end{cases}\\] Define a function that computes the factorial of a given non-negative integer \\(n\\) using iteration by while. def...","url": "https://umutozge.github.io/docs/_pages/exercises/iteration.html"
+  },{
+    "title": "Exercises on Number Theory",
+    "excerpt":"List all the divisors, proper divisors, and prime divisors of 28. Provide a definition of a composite number using the concept of proper divisors. Here is a definition: Definition 1 (trivial divisors) Given an integer \\(n\\), we say that 1,-1,\\(n\\), and \\(-n\\) are the trivial divisors of \\(n\\). Define prime...","url": "https://umutozge.github.io/docs/_pages/exercises/numbers.html"
+  },{
+    "title": "COGS 501 Quiz",
+    "excerpt":"[2pts*] Translate the following into first order logic: i. Every student in this class has submitted at least one assignment. - S(x): “x is a student in this class” - A(x, y): “x has submitted assignment y” \\[\\forall x (S(x) \\to \\exists y A(x,y))\\] ii. There is a professor who...","url": "https://umutozge.github.io/docs/q06s/"
+  },{
+    "title": "Schedule",
+    "excerpt":"Week Reading Exercises Posts Quiz :calendar: 2/10         :calendar: 9/10         :calendar: 23/10         :calendar: 30/10         :calendar: 6/11       quiz :calendar: 13/11 CP 1.5, Epp 3, sections on Prolog can be omitted. 1-5 of Iteration...","url": "https://umutozge.github.io/docs/schedule/"
+  },{
+    "title": "Syllabus",
+    "excerpt":"Prerequisites The course is open, without any prerequisites, to any graduate student, with Cogsci first, Informatics second, and the rest third priority. Material Composing Programs Epp, Susanna S. (2018). Discrete Mathematics with Applications. 5th ed., Cengage Learning. Everything that is discussed in the class. Requirements Weekly quizzes Midterm exam Final...","url": "https://umutozge.github.io/docs/syllabus/"
+  },{
+    "title": "Posts by Tag",
+    "excerpt":" ","url": "https://umutozge.github.io/docs/tags/"
+  },{
+    "title": "Posts by Year",
+    "excerpt":" ","url": "https://umutozge.github.io/docs/posts/"
+  }]

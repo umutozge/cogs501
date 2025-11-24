fdb0b9273fd6e99448c5c374c37bb098f1d234cf
Umut Özge <umut.oezge@gmail.com>
2025-11-24 11:43:45 +0300

    perma

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -24,7 +24,7 @@ var store = [{
     "excerpt":"Define a function apply_twice that takes a function f and a value x, and returns the result of applying f to x two times. def sqr(x): return x * x def apply_twice(f, x): \"\"\"apply f to x twice. &gt;&gt;&gt; apply_twice(sqr, 3) 81 \"\"\" Define a function applier that applies a...","url": "http://localhost:4000/cogs501/exercises/higher-order-functions/"
   },{
     "title": "Programming exercises on iteration",
-    "excerpt":"The factorial function is defined over the non-negative integers as:1 \\[n! = \\begin{cases} 1, &amp; \\text{if } n = 0;\\\\ n \\times (n - 1)!, &amp; \\text{if } n &gt; 0. \\end{cases}\\] Define a function that computes the factorial of a given non-negative integer \\(n\\) using iteration by while. def...","url": "http://localhost:4000/cogs501/exercises/iteration/"
+    "excerpt":"Permalink: /exercises/iteration/ The factorial function is defined over the non-negative integers as:1 \\[n! = \\begin{cases} 1, &amp; \\text{if } n = 0;\\\\ n \\times (n - 1)!, &amp; \\text{if } n &gt; 0. \\end{cases}\\] Define a function that computes the factorial of a given non-negative integer \\(n\\) using iteration by...","url": "http://localhost:4000/cogs501/exercises/iteration/"
   },{
     "title": "Exercises on Number Theory",
     "excerpt":"List all the divisors, proper divisors, and prime divisors of 28. Provide a definition of a composite number using the concept of proper divisors. Here is a definition: Definition 1 (trivial divisors) Given an integer \\(n\\), we say that 1,-1,\\(n\\), and \\(-n\\) are the trivial divisors of \\(n\\). Define prime...","url": "http://localhost:4000/cogs501/exercises/numbers/"
@@ -33,7 +33,7 @@ var store = [{
     "excerpt":"[2pts*] Translate the following into first order logic: i. Every student in this class has submitted at least one assignment. - S(x): “x is a student in this class” - A(x, y): “x has submitted assignment y” \\[\\forall x (S(x) \\to \\exists y A(x,y))\\] ii. There is a professor who...","url": "http://localhost:4000/cogs501/q06s/"
   },{
     "title": "Schedule",
-    "excerpt":"Week Reading Exercises Posts Quiz :calendar: 2/10         :calendar: 9/10         :calendar: 23/10         :calendar: 30/10         :calendar: 6/11       quiz :calendar: 13/11 CP 1.5, Epp 3, sections on Prolog can be omitted. 1-5 of Iteration...","url": "http://localhost:4000/cogs501/schedule/"
+    "excerpt":"Week Reading Exercises Posts Quiz :calendar:2/10         :calendar:9/10         :calendar:23/10         :calendar:30/10         :calendar:6/11       quiz :calendar:13/11 CP 1.5, Epp 3, sections on Prolog can be omitted. 1-5 of Iteration Iteration by while no need for...","url": "http://localhost:4000/cogs501/schedule/"
   },{
     "title": "Syllabus",
     "excerpt":"Prerequisites The course is open, without any prerequisites, to any graduate student, with Cogsci first, Informatics second, and the rest third priority. Material Composing Programs Epp, Susanna S. (2018). Discrete Mathematics with Applications. 5th ed., Cengage Learning. Everything that is discussed in the class. Requirements Weekly quizzes Midterm exam Final...","url": "http://localhost:4000/cogs501/syllabus/"
